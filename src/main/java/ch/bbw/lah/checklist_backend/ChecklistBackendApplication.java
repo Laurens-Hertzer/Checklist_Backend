a127b36b5ac479fe6e3f9bded2d17b5577a68d74
Laurens-Hertzer <laurens.hertzer@stud.kbw.ch>
2026-08-28 11:03:37 +0200

    Initial commit

---
+++ src/main/java/ch/bbw/lah/checklist_backend/ChecklistBackendApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.lah.checklist_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChecklistBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChecklistBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/bbw/lah/checklist_backend/ChecklistBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.lah.checklist_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChecklistBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
